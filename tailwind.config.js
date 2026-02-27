4f872be8d1e6a2c9d9a3f0e7c08ddf99814e34d5
urielarellano <urielarellano017@gmail.com>
2026-02-27 10:44:06 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#7C9AC0',
+        peach: '#FF8C72',
+        lightPeach: '#FFA07A',
+      }
+    },
+  },
+  plugins: [],
+}
